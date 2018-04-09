5bbb6ff48bb89233e9595bca61c26d1113ffd93f
TejuKandula <tejaswikandula@unomaha.edu>
2018-04-08 22:45:24 -0500

    first commit

--- resources/views/patient/active_record.blade.php
+++ resources/views/patient/active_record.blade.php
@@ -12,9 +12,9 @@
         </style>
 
     {{--@parent--}}
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
 
     {{--Three Panels--}}
     <div class="container-fluid" style="margin-top: 0;padding-top: 0;padding-left: 1%;">
