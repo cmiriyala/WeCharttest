224f1101a4aa368204a0be8359e377b8d1f77da2
TejuKandula <tejaswikandula@unomaha.edu>
2018-04-08 22:56:56 -0500

    first commit

--- resources/views/admin/addMedia.blade.php
+++ resources/views/admin/addMedia.blade.php
@@ -26,9 +26,8 @@
 
 @extends('layouts.app')
 @section('content')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 
 
