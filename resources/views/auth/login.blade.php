350b0860b1a80edcb9f296392df7f99f096cb6bb
TejuKandula <tejaswikandula@unomaha.edu>
2018-04-18 21:20:53 -0500

    new ui change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -262,7 +262,7 @@
         </script>
 
         @else
-            <div class="col-md-1 col-md-offset-5" style="margin-left:7vw;margin-top:36vh;position: absolute">
+            <div class="col-md-1 col-md-offset-5" style="margin-left:6vw;margin-top:36vh;position: absolute">
                 <a href="#aboutus" class="window">About WeChart</a><br>
                 <div id="aboutus" class="popup">
                     <p style="text-align: center; font-size: 120%">About WeChart</p>
@@ -276,7 +276,7 @@
                 </div>
             </div>
             </div>
-            <div class="col-md-1 col-md-offset-5" style="margin-left:18vw;margin-top:36vh;position: absolute">
+            <div class="col-md-1 col-md-offset-5" style="margin-left:17vw;margin-top:36vh;position: absolute">
                 <a href="#login-box" class="window">Contact Us</a><br>
                 <div id="login-box" class="popup2">
                     <p style="text-align: center; font-size: 120%">Contact Us</p>
