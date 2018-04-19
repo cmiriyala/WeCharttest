438ca91a1f8d724a558d6f7a159978f9774372ed
TejuKandula <tejaswikandula@unomaha.edu>
2018-04-18 21:49:13 -0500

    new ui change6

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -252,7 +252,7 @@
             <div class="col-md-1 col-md-offset-5" style="margin-left:6%;margin-top:33.5vh;position: fixed">
                 <a href="#login-box" class="login-window">About WeChart</a><br>
             </div>
-            <div class="col-md-1 col-md-offset-5" style="margin-left:17.5%;margin-top:33.5vh;position: fixed">
+            <div class="col-md-1 col-md-offset-5" style="margin-left:17.2%;margin-top:33.5vh;position: fixed">
                 <a href="#">Contact us</a>
             </div>
         <script>
